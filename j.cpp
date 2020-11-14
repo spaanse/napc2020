03157d28c7d19cab3ccb28a208ff5d3e850bb7f7
Robin Holen <Holenrobin@gmail.com>
2020-11-14 13:54:34 +0100

    j

--- j.cpp
+++ j.cpp
@@ -6,7 +6,13 @@ int main(){
     long long no_of_jars;
     long long capacity;
     cin >> no_of_jars >> capacity;
-
-    long long jars_in_boxes = no_of_jars % capacity;
-    cout << jars_in_boxes;    
+    
+    if((no_of_jars % capacity) == 0){
+        cout << (no_of_jars / capacity) << endl;
+    }
+    else{
+        long long boxes = no_of_jars / capacity + 1;
+        long long minimum_in_box = no_of_jars / boxes;
+        cout << minimum_in_box << endl;
+    }
 }
